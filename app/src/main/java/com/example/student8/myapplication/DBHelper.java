3baf2baae2b59e23b9e9b2a7d10d72546aea6877
Monster155 <damir180501@mail.ru>
2016-10-16 17:16:44 +0300

    Commit Message Error

--- app/src/main/java/com/example/student8/myapplication/DBHelper.java
+++ app/src/main/java/com/example/student8/myapplication/DBHelper.java
@@ -4,11 +4,31 @@ import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper{
-    public static final String TABLE_NAME = "CONTACT";
-    public static final String NAME = "FIRST_NAME";
-    public static final String PHONE = "PHONE";
+    // имя базы данных
+    private static final String DATABASE_NAME = "spdatabase.db";
+    // версия базы данных
+    private static final int DATABASE_VERSION = 1;
+    // имя таблицы
+    private static final String DATABASE_TABLE = "cats";
+    // названия столбцов
+    public static final String CAT_NAME_COLUMN = "cat_name";
+    public static final String PHONE_COLUMN = "phone";
+    public static final String AGE_COLUMN = "age";
+
+    private static final String DATABASE_CREATE_SCRIPT = "create table "
+            + DATABASE_TABLE + " (" + BaseColumns._ID
+            + " integer primary key autoincrement, " + CAT_NAME_COLUMN
+            + " text not null, " + PHONE_COLUMN + " integer, " + AGE_COLUMN
+            + " integer);";
+
+
+    DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -21,11 +41,18 @@ public class DBHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE_SCRIPT);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Запишем в журнал
+        Log.w("SQLite", "Обновляемся с версии " + oldVersion + " на версию " + newVersion);
 
+        // Удаляем старую таблицу и создаём новую
+        db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_TABLE);
+        // Создаём новую таблицу
+        onCreate(db);
     }
 }
--- app/src/main/java/com/example/student8/myapplication/Dopolnenie.java
+++ app/src/main/java/com/example/student8/myapplication/Dopolnenie.java
@@ -2,6 +2,7 @@ package com.example.student8.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.opengl.ETC1;
 import android.os.Bundle;
@@ -18,11 +19,17 @@ public class Dopolnenie extends Activity {
 
 	Button btn;
 	EditText et;
+	private DBHelper mDatabaseHelper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dopolnenie);
+
+		mDatabaseHelper = new DBHelper(this, "spdatabase.db", null, 1);
+		SQLiteDatabase sdb;
+		sdb = mDatabaseHelper.getReadableDatabase();
+
 		et = (EditText) findViewById(R.id.editText1);
 		btn = (Button) findViewById(R.id.button1);
         btn.setOnClickListener(new View.OnClickListener() {
