8bd2abce2cb61b9766c42792f1e5e3d0fdd1723f
Monster155 <damir180501@mail.ru>
2016-10-16 16:22:13 +0300

    Commit Message Error

--- app/src/main/java/com/example/student8/myapplication/DBHelper.java
+++ app/src/main/java/com/example/student8/myapplication/DBHelper.java
@@ -1,6 +1,7 @@
 package com.example.student8.myapplication;
 
 import android.content.Context;
+import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,6 +12,11 @@ public class DBHelper extends SQLiteOpenHelper{
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                    int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
     }
 
     @Override
