b595f4408654e36116feb1380c731724b9a3d0ac
Monster155 <damir180501@mail.ru>
2016-10-16 18:11:43 +0300

    Blin

--- app/src/main/java/com/example/student8/myapplication/Dopolnenie.java
+++ app/src/main/java/com/example/student8/myapplication/Dopolnenie.java
@@ -1,7 +1,9 @@
 package com.example.student8.myapplication;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.opengl.ETC1;
@@ -20,6 +22,7 @@ public class Dopolnenie extends Activity {
 	Button btn;
 	EditText et;
 	private DBHelper mDatabaseHelper;
+	private SQLiteDatabase mSqLiteDatabase;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +35,37 @@ public class Dopolnenie extends Activity {
 
 		et = (EditText) findViewById(R.id.editText1);
 		btn = (Button) findViewById(R.id.button1);
+		mDatabaseHelper = new DBHelper(this, "mydatabase.db", null, 1);
+
+		mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		// Задайте значения для каждого столбца
+		values.put(DBHelper.CAT_NAME_COLUMN, "Рыжик");
+		values.put(DBHelper.PHONE_COLUMN, "4954553443");
+		values.put(DBHelper.AGE_COLUMN, "5");
+		// Вставляем данные в таблицу
+		mSqLiteDatabase.insert("cats", null, values);
+
         btn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				Cursor cursor = mSqLiteDatabase.query("cats", new String[]{DBHelper.CAT_NAME_COLUMN,
+								DBHelper.PHONE_COLUMN, DBHelper.AGE_COLUMN},
+						null, null,
+						null, null, null);
 
+				cursor.moveToFirst();
+
+				String catName = cursor.getString(cursor.getColumnIndex(DBHelper.CAT_NAME_COLUMN));
+				long phoneNumber = cursor.getLong(cursor.getColumnIndex(DBHelper.PHONE_COLUMN));
+				int age = cursor.getInt(cursor.getColumnIndex(DBHelper.AGE_COLUMN));
+
+				et.setText("Кот " + catName + " имеет телефон " + phoneNumber + " и ему " +
+						age + " лет");
+
+				// не забываем закрывать курсор
+				cursor.close();
 			}
 		});
 	}
