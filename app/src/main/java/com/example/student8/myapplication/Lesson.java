2d5f33e6538a77434b01337ab3103e307a2943e6
student8 <damir180501@mail.ru>
2017-01-16 17:34:41 +0300

    abcde

---
+++ app/src/main/java/com/example/student8/myapplication/Lesson.java
@@ -0,0 +1,36 @@
+package com.example.student8.myapplication;
+
+/**
+ * Created by student8 on 16.01.17.
+ */
+
+public class Lesson {
+    String name;
+    String theme;
+    String text;
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+}
