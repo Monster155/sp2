d36e770629d8ae1fd9307b0208d2870c21b6daf6
Дамир <damir180501@mail.ru>
2017-10-02 18:15:09 +0300

    Расписание косячит. А стоит ли делать в нем дз? может просто рассписания хватит? иначе много строк надо к этому приписать, ну а я же супер лентяй и не буду мучиться с этим, мне проще кинуть всё и пойти играть)))

--- app/src/main/java/com/example/student8/myapplication/TimeTable.java
+++ app/src/main/java/com/example/student8/myapplication/TimeTable.java
@@ -1,14 +1,56 @@
 package com.example.student8.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TimeTable extends Activity{
+
+    Spinner sp[][] = new Spinner[6][8];
+    String choose, tt[];
+    int array, temp, numOfLes = 48;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timetable);
 
+        names();
+        choose = "Я не даун, a Сеня даун";
+
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 8; j++){
+                spiner(sp[i][j]);
+            }
+        }
+
+
+    }
+
+    //поиск id переменных
+    private void names() {
+        tt = new String[numOfLes];
+        for(int i = 0; i < numOfLes; i++){
+            tt[i] = i + "";
+        }
+        numOfLes = 0;
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 8; j++){
+                temp = getResources().getIdentifier(tt[numOfLes], "id", getPackageName());
+                numOfLes++;
+                sp[i][j] = (Spinner) findViewById(temp);
+            }
+        }
+    }
+
+    //заполнение spinner
+    private void spiner(Spinner spin) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add(choose);
+        spin.setAdapter(adapter);
     }
 }
